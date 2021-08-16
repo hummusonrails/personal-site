623b324bff36f23bc3f22e0bcc303d4654a4c2ac
Ben Greenberg <ben@orbit.love>
2021-08-16 17:38:23 +0300

    update posthog init

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,7 +3,7 @@ import 'alpinejs'
 
 // posthost
 import posthog from 'posthog-js'
-posthog.init('phc_6rkVY4selTsUXWY1FqrleCwjqbX1mkm8eCIzBnDo3nh', { api_host: 'http://174.138.125.11' })
+posthog.init('phc_2bu7rZOtIkQ20EKXz0YZ3gW3e3eztG90b9OsQyxUAl5', { api_host: 'https://app.posthog.com' })
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
