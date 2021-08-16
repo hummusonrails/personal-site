dcfeebb313fff6eac07980fe12a6bc9cceefcf0f
Ben Greenberg <ben@orbit.love>
2021-08-16 17:23:00 +0300

    most init

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,6 +1,10 @@
 import 'focus-visible'
 import 'alpinejs'
 
+// posthost
+import posthog from 'posthog-js'
+posthog.init('phc_6rkVY4selTsUXWY1FqrleCwjqbX1mkm8eCIzBnDo3nh', { api_host: 'http://174.138.125.11' })
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
