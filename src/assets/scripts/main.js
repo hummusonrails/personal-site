a052b818205afb8d8c06e5c9e4911db1843327c8
Ben Greenberg <ben@orbit.love>
2021-09-02 09:51:15 -0400

    new relic

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,9 +1,7 @@
 import 'focus-visible'
 import 'alpinejs'
+require('newrelic');
 
-// posthost
-import posthog from 'posthog-js'
-posthog.init('phc_2bu7rZOtIkQ20EKXz0YZ3gW3e3eztG90b9OsQyxUAl5', { api_host: 'https://app.posthog.com' })
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
