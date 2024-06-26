c1ee847e3410633656d1a1b022e601d9780ccd14
Ben Greenberg <hello@yalladevrel.com>
2024-06-26 08:48:31 -0400

    add astro embed to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
+import astroEmbed from 'astro-embed';
+
 import {SITE_METADATA} from "./src/consts.ts";
 
 // https://astro.build/config
 export default defineConfig({
     site: SITE_METADATA.siteUrl,
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap(), tailwind(), astroEmbed()],
     output: 'server',
     adapter: vercel({
         maxDuration: 60,
